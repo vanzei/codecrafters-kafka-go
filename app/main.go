7042ce9ebdbe75dbd69cf1b28acf94502ee815f3
Leonardo Vanzei <leonardovanzei@Leonardos-Mac-mini.local>
2025-08-16 22:41:10 -0400

    pt2 done

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -15,14 +16,39 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	 l, err := net.Listen("tcp", "0.0.0.0:9092")
-	 if err != nil {
-	 	fmt.Println("Failed to bind to port 9092")
-	 	os.Exit(1)
-	 }
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
+	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	if err != nil {
+		fmt.Println("Failed to bind to port 9092")
+		os.Exit(1)
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+
+	defer conn.Close()
+	for {
+		received := bytes.Buffer{}
+		buf := make([]byte, 1024)
+
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			break
+		}
+
+		received.Write(buf)
+
+		conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
+
+	}
 }
